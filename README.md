0a595a95c4420b94e577dbb9630b202837e111db
Martin C. Doege <mdoege@compuserve.com>
2021-04-18 13:20:30 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## HD LED
+
+Shows a HD activity light in the sys tray using Python and GTK 3.
+
+The default block device used is /dev/sda.
+
---
+++ icon.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+fn = "/sys/block/sda/stat"
+ro, wo = 0, 0
+last = ""
+
+import gi, time
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject
+from gi.repository import Gtk
+tray = Gtk.StatusIcon()
+tray.set_visible(True)
+
+def update():
+	global ro, wo, last
+
+	f = open(fn).read()
+	f = f.split()
+	r, w = f[0], f[4]
+	rr, ww = "0", "0"
+	if r != ro:
+		rr = "1"
+	if w != wo:
+		ww = "1"
+	ro, wo = r, w
+	i = f"icon{rr}{ww}.png"
+	if i != last:
+		tray.set_from_file(i)
+		tray.set_visible(True)
+		last = i
+	GObject.timeout_add(100, update)
+
+GObject.timeout_add(100, update)
+Gtk.main()
+
